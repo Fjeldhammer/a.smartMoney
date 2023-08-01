864eb7da53f40ab795f0670413b3adcbd0c4adbf
William Fjeld <thefjeld@gmail.com>
2023-08-01 08:33:13 -0600

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,14 @@
+function displayMessage() {
+    let inputBox = document.getElementById('message');
+
+    let message = inputBox.value;
+
+    Swal.fire(
+        {
+            backdrop: false,
+            title: 'App Name',
+            text: message
+        }
+    );
+}
+
