2895c9600f75749ff831b1e6a2dd82ea85e2d013
Fjeldhammer <fjeld.uilleam@gmail.com>
2023-08-06 17:42:38 -0600

    fixed js code

--- js/site.js
+++ js/site.js
@@ -1,121 +1,145 @@
-// get values
+// get the numbers for the loan out of the inputs
 function getValues() {
-    // get user values from entry point
-    let loanAmount = parseFloat(document.getElementById('loanAmount').value);
-    let totalMonths = parseInt(document.getElementById('totalMonths').value);
-    let interestRate = parseFloat(document.getElementById('interestRate').value);
+    // get each of the values from the inputs on the page
+    let loanAmount = document.getElementById('loanAmount').value;
+    let loanLength = document.getElementById('totalTerm').value;
+    let loanInterest = document.getElementById('interestRate').value;
 
-    // input validation for integers
-    loanAmount = Number(loanAmount);
-    totalMonths = Number(totalMonths);
-    interestRate = Number(interestRate);
+    loanAmount = parseFloat(loanAmount);
+    loanLength = parseInt(loanLength);
+    loanInterest = parseFloat(loanInterest);
 
-    // loop to check if input numbers are actual numbers
-    if (isNaN(loanAmount) == true || isNaN(totalMonths) == true || isNaN(interestRate) == true) {
+    // make sure those values make sense
+    if(isNaN(loanAmount) || isNaN(loanLength) || isNaN(loanInterest)
+            || loanAmount <= 0 || loanLength <= 0 || loanInterest <= 0) {
         Swal.fire({
-            title: 'Oops!',
-            text: 'Unfortunately, SMART MONEY only works with numbers',
             icon: 'error',
+            title: 'OOPS!',
+            text: 'Please enter valid loan details.'
         })
 
     } else {
+        //  do something with those inputs
+        let loanTotals = calculateTotals(loanAmount, loanLength, loanInterest);
 
-        // display the data if everything is ok
-        // assign array to calculation function
-        let loanArray = calculateMortgage(loanAmount, totalMonths, interestRate);
+        displayTotals(loanTotals.monthlyPayment, 
+                      loanAmount,
+                      loanTotals.totalInterest, 
+                      loanTotals.totalCost);
 
-        displayData(loanArray);
-        displayTotals(loanAmount, loanArray);
+        let payments = calculatePayments(loanLength, loanTotals.monthlyPayment, loanAmount, loanInterest);
+
+        displayPayments(payments);
     }
+}
 
+// calculate the totals for the loan
+function calculateTotals(principal, term, rate) {
+    // calculate the monthly payment
+    let monthlyPayment = (principal * (rate / 1200)) / (1 - Math.pow((1 + rate/1200), -term) );
+
+    // calculate the total cost
+    let totalCost = monthlyPayment * term;
+
+    // calculate the total interest
+    let totalInterest = totalCost - principal;
+
+    let loanTotals = {
+        monthlyPayment: monthlyPayment,
+        totalCost: totalCost,
+        totalInterest: totalInterest,
+    };
+
+    return loanTotals;
+}
+
+// display the totals for the loan
+function displayTotals(monthlyPayment, principal, interest, cost) {
+    let formatOptions = {
+        style: 'currency',
+        currency: 'USD'
+    };
+
+    document.getElementById('monthlyPayment').textContent = monthlyPayment.toLocaleString('en-US', formatOptions);
+
+    document.getElementById('totalPrincipal').textContent = principal.toLocaleString('en-US', formatOptions);
+
+    document.getElementById('totalInterest').textContent = interest.toLocaleString('en-US', formatOptions);
+
+    document.getElementById('totalCost').textContent = cost.toLocaleString('en-US', formatOptions);
 
 }
 
-// calculate mortgage
-function calculateMortgage(loanAmount, totalMonths, interestRate) {
+// calculate each month of payments in the table
+function calculatePayments(term, monthlyPayment, principal, rate) {
+    // create for loop to calculate every month of payments
 
-    // state calculation items
-    let loanArray = [];
+    let remainingBalance = principal;
     let totalInterest = 0;
-    let balanceRemaining = loanAmount
+    let paymentsArray = [];
 
-    // total monthly payment
-    monthlyPayment = (loanAmount) * (interestRate / 1200) / (1 - (1 + interestRate / 1200) ** (-totalMonths));
+    for(let month = 1; month <= term; month += 1) {
 
-    for (let month = 1; month <= totalMonths; month++) {
+        // -- calculate each column of my table
+        let interestPayment = remainingBalance * rate / 1200;
 
-        // interest payment
-        let monthlyInterest = balanceRemaining * interestRate / 1200;
+        let principalPayment = monthlyPayment - interestPayment;
 
-        // principal payment
-        let monthlyPrincipal = monthlyPayment - monthlyInterest;
+        totalInterest += interestPayment;
 
-        // accumulated total interest
-        totalInterest += monthlyInterest;
+        remainingBalance -= principalPayment;
 
-        // remaining balance for next month
-        balanceRemaining = balanceRemaining + monthlyInterest - monthlyPayment
-        if (balanceRemaining <= 0){
-            balanceRemaining = 0;
-        }
-
-        let newPayment = {
+        // -- create an object to store those values
+        let loanPayment = {
             month: month,
             payment: monthlyPayment,
-            principal: monthlyPrincipal,
-            monthlyInterest: monthlyInterest,
+            principal: principalPayment,
+            interest: interestPayment,
             totalInterest: totalInterest,
-            balance: balanceRemaining,
+            balance: remainingBalance
+        };
+        // -- put that object in an array
+        paymentsArray.push(loanPayment);
+    }
+    // return that array
+    return paymentsArray;
+}
+
+// display each month of payments in the table
+function displayPayments(payments) {
+    let formatOptions = {
+        style: 'currency',
+        currency: 'USD'
+    };
+
+    const tableRowTemplate = document.getElementById('payment-row'); // the <template> element with one <tr> inside
+    const paymentsTable = document.getElementById('loan-table'); // the <tbody> element
+
+    paymentsTable.innerHTML = ''; // empty out the table contents before making new table rows
+
+    for(let i = 0; i < payments.length; i++) {
+
+        let currentPayment = payments[i];
+
+        let tableRow = tableRowTemplate.content.cloneNode(true); // copy the <tr> element from the <template>
+
+        let tableCells = tableRow.querySelectorAll('td'); // gets ALL the <td> elements inside the <tr>
+
+        // assign the content of each <td> to the properties of our object
+        tableCells[0].textContent = currentPayment.month.toLocaleString('en-US');
+        tableCells[1].textContent = currentPayment.payment.toLocaleString('en-US', formatOptions);
+        tableCells[2].textContent = currentPayment.principal.toLocaleString('en-US', formatOptions);
+        tableCells[3].textContent = currentPayment.interest.toLocaleString('en-US', formatOptions);
+        tableCells[4].textContent = currentPayment.totalInterest.toLocaleString('en-US', formatOptions);
+        tableCells[5].textContent = Math.abs(currentPayment.balance).toLocaleString('en-US', formatOptions);
+
+        if (i == payments.length -1) {
+            tableRow.querySelector('tr').classList.add('table-success');
         }
 
-        loanArray.push(newPayment);
+        // put the <tr> in the <tbody>
+        paymentsTable.appendChild(tableRow);
     }
-    return loanArray;
-}
-
-
-// display data
-function displayData(loanArray) {
-
-    // find the table on the page
-    const loanTable = document.getElementById('loan-table');
-
-    // find the table row template
-    const loanTemplate = document.getElementById('loan-table-template');
-
-    //clear out the table
-    loanTable.innerHTML = '';
-
-    //for each month:
-    for (let index = 0; index < loanArray.length; index += 1) {
-        // -- get one month
-        let month = loanArray[index];
-
-        // -- clone the template
-        let tableRow = loanTemplate.content.cloneNode(true);
-
-        // -- get each property of event
-        // -- insert each property into the cloned template
-        tableRow.querySelector('[data-id="month"]').innerText = month.month;
-        tableRow.querySelector('[data-id="payment"]').innerText = month.payment;
-        tableRow.querySelector('[data-id="principal"]').innerText = month.principal;
-        tableRow.querySelector('[data-id="interest"]').innerText = month.monthlyInterest;
-        tableRow.querySelector('[data-id="totalInterest"]').innerText = month.totalInterest;
-        tableRow.querySelector('[data-id="balance"]').innerText = month.balance;
-
-        // -- insert the event data into table
-        loanTable.appendChild(tableRow);
-    }
-}
-
-// display totals
-function displayTotals(loanAmount, loanArray) {
-
-
-    document.getElementById('monthlyPayment').innerHTML = loanArray[0].payment.toLocaleString();
-    document.getElementById('totalPrincipal').innerHTML = loanAmount.toLocaleString();
-    document.getElementById('totalInterest').innerHTML = loanArray[loanArray.length-1].totalInterest.toLocaleString();
-    document.getElementById('totalCost').innerHTML = (loanAmount + loanArray[loanArray.length-1].totalInterest).toLocaleString();
 
 }
 
